9c73187d16cd92ea84d3227bfc8a10f0183449d6
KristelCharm <kristelcharmcamano.25@gmail.com>
2026-08-09 01:53:50 +0800

    Information

---
+++ INFORMATION.java
@@ -0,0 +1,26 @@
+public class INFORMATION {
+    public static void main(String[] args){
+    
+    // variable
+    String pet = "🐶";
+    String name = "Kristel Charm T. Camano";
+    String nickname = "Star, Tala, Charmie,Chami";
+    String birthday = "March 25, 2007";
+    String address = "Brgy. Bancal, Alimodia, Iloilo";
+    String favsong = "Dito, Sa Ilalim Ng Buwan by: Arthur Miguel, and Torete by: Moira";
+    String motivation = "What keeps me moving forward is the dream of building a future where I can give my loved ones the comfort, happiness, and opportunities they deserve. Every challenge I overcome today is a step closer to becoming, someone who can bring hope, kindness, and meaningful change to the lives of others. I believe that true, success is not measured by what I achieve for myself, but by how many people I can uplift and,inspire along the way. No matter how difficult the journey becomes, I will keep moving forward with love in my heart, purpose in my soul, and faith in the future I am building."; 
+    String support = "This semester would be more comfortable for me if the instructions for activities and projects were clear and easy to follow. I also appreciate enough time to complete tasks so I can learn, improve my skills and produce quality work";
+
+    System.out.println("===================================================================================================================================");
+    System.out.println(" I am " + pet + " person ");
+    System.out.println("===================================================================================================================================");
+    System.out.println("===================================================================================================================================");
+    System.out.println(" Name            : " + name + " / " + nickname);
+    System.out.println(" Birthday        : " + birthday);
+    System.out.println(" Address         : " + address);
+    System.out.println(" Favorite Song   : " + favsong);
+    System.out.println(" Motivation      : " + motivation);
+    System.out.println("Support          : " + support);
+    System.out.println("===================================================================================================================================");
+    }
+}
